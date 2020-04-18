02bf6c14125116f4e6563dbc9430646e39eca58c
reemas-gemengserv <reemas.gem@gmail.com>
2020-04-18 16:47:15 +0530

    Add packages, services in recipe project

--- src/main/java/guru/springframework/recipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/recipeapp/controllers/IndexController.java
@@ -1,14 +1,24 @@
 package guru.springframework.recipeapp.controllers;
 
+import guru.springframework.recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    private final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
     @RequestMapping({"","/","/index"})
-    public  String getIndexPage(){
-        System.out.println("Some messags to say.....12345 55555");
+    public  String getIndexPage(Model model){
+
+    model.addAttribute("recipes", recipeService.getRecipes());
+
         return "index";
     }
 }
